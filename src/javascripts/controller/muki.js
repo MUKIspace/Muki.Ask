b20b7f6ff0535907940baf97dd15310db9e94767
Dca <dca35311@gmail.com>
2014-02-21 10:03:31 +0800

    Update simple Pagination

--- src/javascripts/controller/muki.js
+++ src/javascripts/controller/muki.js
@@ -8,8 +8,14 @@ controller('MukiCtrl', function(
     //
     $scope.q = [];
     $scope.qlength = 0;
+    $scope.qPage = 0;
+    $scope.pageItems = 10;
 
     console.log('ng run');
+
+    $scope.uPage = function updatePage(index) {
+        $scope.qPage = parseInt(index, 10);
+    }
 
     Ask.get().then(function (results) {
         $scope.q        = results.data;
