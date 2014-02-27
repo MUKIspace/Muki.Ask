a0c828b7749bc7f86fcf3be708e3ae64a5915a24
Dca <dca35311@gmail.com>
2014-02-27 16:34:10 +0800

    Update publish commit message with Date

--- gruntfile.js
+++ gruntfile.js
@@ -3,7 +3,8 @@ module.exports = function (grunt) {
     grunt.initConfig({
         'gh-pages': {
             options: {
-                base: 'build'
+                base: 'build',
+                message: 'Updates on ' + ( (new Date()).toString() )
             },
             src: '**/*'
         },
